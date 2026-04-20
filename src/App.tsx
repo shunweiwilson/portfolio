f90184771fbc70243872df8cf62bfc791684f036
Wilson Shun-wei Wu <shunweiwilson@gmail.com>
2026-04-20 02:01:28 -0700

    add analytic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Menu, X, ArrowLeft, ArrowRight } from 'lucide-react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // --- Shared Reusable Components ---
 
